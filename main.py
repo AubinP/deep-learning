dc2ca3a3a817f926a4e793fb297afd17cfae3073
Aubin Porte <aubin.porte@gmail.com>
2022-08-14 21:36:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,134 @@
+"""
+Matrice confusion : [[131837  28319]
+                    [ 29520 130324]]
+Accuracy : 0.819253
+Recall : 0.815320
+Precision : 0.821492
+"""
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Embedding, Dropout, LSTM
+
+
+SEQUENCE_LENGTH = 150  # Taille maximale d'une phrase
+EMBEDDING_DIM = 200  # Dimension de la matrice d'embedding GloVe
+BATCH_SIZE = 32  # Batch size du fir
+
+
+if __name__ == '__main__':
+    pd.set_option('display.max_columns', None)
+    df = pd.read_csv('proper_df.csv', index_col=0)
+    print(df.head())
+
+    X = df['text']
+    X = X.astype(str)
+
+    y = df['polarity']
+    y = y.map(lambda x: 1 if x == 4 else x)
+    y = pd.get_dummies(y).values  # Equivalent à la fonction one_hot de Keras
+
+    print(X.shape)
+    print(y.shape)
+
+    # Entrainement du tokenizer
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(X)
+    vocab_size = len(tokenizer.word_index) + 1
+
+    # Remplace le texte par une séquence d'entiers
+    X = tokenizer.texts_to_sequences(X)
+    X = pad_sequences(X, maxlen=SEQUENCE_LENGTH, padding='post')
+
+    # Chargement de la matrice d'embeddings
+    embeddings_index = dict()
+    f = open('glove.6B/glove.6B.'+str(EMBEDDING_DIM)+'d.txt', encoding="utf8")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    print('Loaded %s word vectors.' % len(embeddings_index))
+
+    # Initialisation de la matrice d'embeddings avec les mots du dictionnaires (présent dans le tokenizer)
+    embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
+    for word, i in tokenizer.word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    # Split des dataframes
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
+
+    # Model
+    model = Sequential()
+    model.add(Embedding(vocab_size, EMBEDDING_DIM, weights=[embedding_matrix], input_length=SEQUENCE_LENGTH, trainable=False))
+    model.add(LSTM(50, input_shape=(SEQUENCE_LENGTH, 1), return_sequences=True))
+    # Return_sequence True renvoie 3D donc activer Flatten, sinon renvoie 2D donc commenter Flatten
+    model.add(Flatten())
+    model.add(Dropout(0.5))  # Réduire l'overfitting
+    model.add(Dense(2, activation='sigmoid'))  # 1 sortie par classes
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    print(model.summary())
+
+    history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=15, verbose=1, validation_data=(X_val, y_val),
+                        shuffle=True)
+    model.save("modelddd.h5")
+
+    try:
+        # Graphique d'évlution de l'accuracy par epochs
+        plt.subplot(1)
+        plt.title('Accuracy')
+        plt.plot(history.history['accuracy'], label='train')
+        plt.plot(history.history['val_accuracy'], label='test')
+        plt.legend(loc='upper left')
+        plt.savefig('accuracy.png')
+        plt.clf()
+        # Graphique d'évlution de la perte par epochs
+        plt.subplot(2)
+        plt.title('Loss')
+        plt.plot(history.history['loss'], label='train')
+        plt.plot(history.history['val_loss'], label='test')
+        plt.legend(loc='upper left')
+        plt.savefig('loss.png')
+        plt.clf()
+    except:
+        pass
+
+    y_test = np.argmax(y_test, axis=1)
+    y_pred = np.argmax(model.predict(X_test), axis=1)
+
+    # Matrice de confusion
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print(conf_matrix)
+    tn, fp, fn, tp = conf_matrix.ravel()
+    plt.clf()
+    plt.imshow(conf_matrix, interpolation='nearest')
+    classNames = ['Negative', 'Positive']
+    plt.title('Classification Confusion Matrix on Test Data')
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    tick_marks = np.arange(len(classNames))
+    plt.xticks(tick_marks, classNames, rotation=45)
+    plt.yticks(tick_marks, classNames)
+    s = [['TN', 'FP'], ['FN', 'TP']]
+    for i in range(2):
+        for j in range(2):
+            plt.text(j, i, str(s[i][j]) + " = " + str(conf_matrix[i][j]))
+    plt.savefig('confusion matrix.png')
+
+    # Métriques dévaluations du modèle
+    print('Accuracy : %f' % ((tn + tp) / (tn + fp + fn + tp)))
+    print('Recall : %f' % (tp / (fn + tp)))
+    print('Precision : %f' % (tp / (fp + tp)))
---
+++ main_functiuns.py
@@ -0,0 +1,206 @@
+"""
+Source:
+    - Aller plus loin avec le preprocessing: https://nlp.stanford.edu/projects/glove/preprocess-twitter.rb
+    - Keras docs:
+        - https://keras.io/preprocessing/text/#tokenizer
+        - https://keras.io/preprocessing/sequence/#pad_sequences
+        - https://keras.io/layers/embeddings/#embedding
+        - https://keras.io/layers/recurrent/#lstm
+        - https://keras.io/metrics/
+        - https://keras.io/activations/
+    - embedding layer : Jeffrey Pennington, Richard Socher, and Christopher D. Manning. 2014. GloVe: Global Vectors for
+        Word Representation. (https://nlp.stanford.edu/projects/glove/)
+"""
+
+import pickle
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Embedding, Reshape, LSTM, GRU, Dense, Flatten, Dropout, Conv1D, MaxPooling1D
+from tensorflow.keras.utils import plot_model, to_categorical
+
+
+EMBEDDING_DIM = 200  # 50, 100, 200 ou 300
+MAX_SEQUENCE_LENGTH = 150
+BATCH_SIZE = 64
+TRAIN = True  # True pour entrainer le modèle, False pour juste le charger
+
+
+def padding_data(sequences):
+    """
+    Ajout de zéros pour uniformiser la taille des listes
+    :param sequences: listes de données
+    :param max_length: taille que doit faire la liste
+    :return: données uniformisées
+    """
+    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH, padding='post')
+    return data
+
+
+def load_embedding_model():
+    """
+    Chargement du modèle pré entrainée GloVe
+    """
+    embeddings_index = dict()
+    f = open('glove.6B/glove.6B.'+str(EMBEDDING_DIM)+'d.txt', encoding="utf8")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    print('Loaded %s word vectors.' % len(embeddings_index))
+    return embeddings_index
+
+
+def get_embedding_matrix(vocab_size, tokenizer):
+    """
+
+    :param vocab_size: Taille du vocabulaire
+    :param tokenizer: Tokenizer (déjà entrainé)
+    :return: Matrice de plongement de mots
+    """
+    embeddings_index = load_embedding_model()
+    embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
+    for word, i in tokenizer.word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+    print('Embedding matrix ready')
+    return embedding_matrix
+
+
+def build_model_lstm(max_fatures, dim_embedding, embedding_matrix):
+    """
+    Création du modèle
+    :param input_dim: Taille du vecteurs d'entrée
+    :param output_dim: Taille du vecteurs de sortie
+    :param embedding_matrix: Matrice de plongement de mots
+    :return: Structure du modèle
+    """
+    model = Sequential()
+    model.add(Embedding(max_fatures, dim_embedding, weights=[embedding_matrix], input_length=MAX_SEQUENCE_LENGTH,
+                        trainable=False))
+    model.add(LSTM(100, return_sequences=False))
+    model.add(Dense(64))
+    # model.add(Flatten())
+    # model.add(Dropout(0.5))
+    model.add(Dense(1, activation='relu'))
+    return model
+
+
+def save_model_weights(model):
+    """
+    :param model:
+    :return:
+    """
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+
+
+def plot_fit_history(history):
+    # summarize history for accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig('accuracy.png')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig('loss.png')
+    plt.show()
+
+
+if __name__ == '__main__':
+    pd.set_option('display.max_columns', None)
+    df = pd.read_csv('proper_df.csv', index_col=0)
+    print(df.head())
+
+    # X= df['text_lemmatizer']
+    X = pd.concat([df['text_lemmatizer'][0:100000], df['text_lemmatizer'][860000:960000]])
+
+    # y = df['polarity']
+    y = pd.concat([df['polarity'][0:100000], df['polarity'][860000:960000]])
+    y = y.map(lambda x: 1 if x == 4 else x)
+    y = to_categorical(y, num_classes=2)
+    # y = pd.get_dummies(y).values
+    print(y)
+
+    # Tokenization du texte
+    if os.path.isfile('tokenizer.pickle'):
+        with open('tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+        print('Tokenizer loaded')
+    else:
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(X)
+        print('Tokenizer fit on texts')
+        with open('tokenizer.pickle', 'wb') as handle:
+            pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    vocab_size = len(tokenizer.word_index) + 1
+    print('Vocabulary size:', vocab_size)
+
+    # Transformation des mots en nombres par le tokenizer
+    X = tokenizer.texts_to_sequences(X)
+
+    # Uniformisation de la taille du tableau. /!\ >> A voir avec la moyenne/médiane de la longueur des tweets.
+    print(len(X[0]))
+    X = padding_data(X)
+    print(len(X[0]))
+
+    # Separation du dataset en train, test, validation
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    print(y_train)
+    print(X_train.shape, y_train.shape)
+    print(X_test.shape, y_test.shape)
+
+    if TRAIN:
+        embedding_matrix = get_embedding_matrix(vocab_size, tokenizer)
+
+        model = build_model_lstm(max_fatures=vocab_size, dim_embedding=EMBEDDING_DIM, embedding_matrix=embedding_matrix)
+        # model = build_model_conv(max_fatures=vocab_size, dim_embedding=EMBEDDING_DIM, embedding_matrix=embedding_matrix)
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        print(model.summary())
+        try:
+            # Download GraphViz to save PNG (https://graphviz.gitlab.io/_pages/Download/windows/graphviz-2.38.msi)
+            plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+        except:
+            print("Can't save the model as an image")
+        history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=10, verbose=0)
+        plot_fit_history(history)
+        model.save("model.h5")
+        print("Model saved")
+    else:
+        model = load_model('model.h5')
+        model.summary()
+        print("Model loaded")
+
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=1)
+    print('Accuracy: %f' % (accuracy * 100))
+    print('Loss: %f' % (loss * 100))
+
+    y_pred = model.predict(X_test)
+    print(log_loss(y_test, y_pred, eps=1e-15))
